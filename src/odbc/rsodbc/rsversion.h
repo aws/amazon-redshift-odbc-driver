6e99dde95bee8e970a971ca29f5fde49f8149007
Vahid Saber Hamishagi <vahidsbr@amazon.de>
2023-02-09 17:11:21 +0100

    Version Adjustments 2.0.0.3

--- src/odbc/rsodbc/rsversion.h
+++ src/odbc/rsodbc/rsversion.h
@@ -1,5 +1,5 @@
 #define ODBC_DRIVER_VERSION "02.00.0000"
-#define FILEVER        2,0,0,0
-#define PRODUCTVER     2,0,0,0
-#define STRFILEVER     "2, 0, 0, 0\0"
-#define STRPRODUCTVER  "2, 0, 0, 0"
+#define FILEVER        2,0,0,3
+#define PRODUCTVER     2,0,0,3
+#define STRFILEVER     "2, 0, 0, 3\0"
+#define STRPRODUCTVER  "2, 0, 0, 3"
